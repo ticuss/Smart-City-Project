7bd07a6c8bf11d699f9e4e7c169bc167aa54f55f
ticuss <ticu369@gmail.com>
2022-05-17 15:56:04 +0200

    fix: delete unused import

--- dashboard/index.py
+++ dashboard/index.py
@@ -2,7 +2,6 @@ from zipfile import ZipFile
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
 import plotly.express as px
